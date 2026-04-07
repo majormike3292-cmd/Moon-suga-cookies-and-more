c953f42b83f96bb09bbf407d8062c4b1594908fb
majormike3292-cmd <majormike3292@gmail.com>
2026-04-07 19:22:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function order() {
+  alert("🍪 Order placed! Fresh cookies coming soon!");
+}
 
+let ovenOn = false;
+
+setInterval(() => {
+  ovenOn = !ovenOn;
+  document.getElementById("status").innerText = ovenOn ? "ON 🔥" : "OFF ❄️";
+}, 3000);
